e57b4730a3fb9225389fdd36750c89c2f8b6fcd8
flyaway46 <1659084227@qq.com>
2015-03-03 17:42:48 +0800

    用户修改信息功能补全

--- Admin/Lib/Action/UserAction.class.php
+++ Admin/Lib/Action/UserAction.class.php
@@ -9,7 +9,7 @@ class UserAction extends AdminCommonAction {
 		$m=M('User');
 		import('ORG.Util.Page');
 		$count=$m->where('role=1 OR role=2 OR role=3')->order('id desc')->count();
-		$page  = new Page($count,10);
+		$page  = new Page($count,5);
 		$page->setConfig('header','条记录');
 		$show=$page->show();
 		$arr=$m->limit($page->firstRow.','.$page->listRows)->where('role=1 OR role=2 OR role=3')->order('id desc')->select();
@@ -22,7 +22,7 @@ class UserAction extends AdminCommonAction {
 		$m=M('User');
 		import('ORG.Util.Page');
 		$count=$m->where('role=0')->order('id desc')->count();
-		$page  = new Page($count,10);
+		$page  = new Page($count,5);
 		$page->setConfig('header','条记录');
 		$show=$page->show();
 		$arr=$m->limit($page->firstRow.','.$page->listRows)->where('role=0')->order('id desc')->select();
@@ -180,8 +180,8 @@ class UserAction extends AdminCommonAction {
 		$where['id']=$yid;
 		$field=$m->find($id);
 		$yrole=$m->where($where)->find();
-		$tem=$field;		
-		if($yrole['role'] != "9" && ($tem['role']=='2' || $tem['role']=='3' || $tem['role']=='9')){
+		$tem=$field;
+		if($yrole['role'] != "9" && $field['role']=array('in','2,3,9')){
 			echo"<script type='text/javascript'>alert('权限不足，操作被拒绝！');history.back(-1);</script>";
 		}else{
 			$field['role']='5';
@@ -190,7 +190,6 @@ class UserAction extends AdminCommonAction {
 			$tem['delinfo']=$delinfo;
 			$n->add($tem);
 			$this->redirect('userview');
-			
 		};
 	}	
 
@@ -218,11 +217,11 @@ class UserAction extends AdminCommonAction {
 		$yrole=$m->where($where)->find();
 		$t['id']=array('eq',"$id");
 		$myrole=$m->where($t)->order('id desc')->getField('role');
-		if($yrole['role'] != "9" && ($tem['role']=='2' || $tem['role']=='3' || $tem['role']=='9')){
+		if($yrole['role'] != "9" && $myrole=array('in','2,3,9')){
 			echo"<script type='text/javascript'>alert('权限不足，操作被拒绝！');history.back(-1);</script>";
 		}else{
 			$count=$m->where($t)->setField('password',MD5('MRIcenter'));
-			$this->redirect('userview');		
+			$this->display('userview');		
 		};
 	}
 
@@ -234,24 +233,43 @@ class UserAction extends AdminCommonAction {
 			echo 'n';	
 		}
 	}
-
-	public function role(){
+	public function modify(){
 		$m=M('User');
-		$id=$_POST['role_ed'];
-		$role=$_POST['usertype'];
-		$yid=$_SESSION['id'];
-		$where['id']=$yid;
-		$field=$m->find($id);
-		$yrole=$m->where($where)->find();
-		$sys=$field;
-		if($yrole['role'] != "9"){
-			echo"<script type='text/javascript'>alert('权限不足，操作被拒绝！');history.back(-1);</script>";
-		}else if(!isset($role) || $role == ""){
-			echo"<script type='text/javascript'>alert('你未选择用户权限！');history.back(-1);</script>";
+		$condition_user['id']=$_POST['id'];
+		$condition['username']=$_POST['username'];
+		$condition['principal']=$_POST['principal'];
+		$condition['department']=$_POST['department'];
+		$condition['unit']=$_POST['unit'];
+		$condition['truename']=$_POST['truename'];
+		$condition['sex']=$_POST['sex'];	
+		//学历判别;2博士;1硕士
+		$condition['mail']=$_POST['mail'];
+		if($_POST['grade']!=''){
+			$condition['grade']=$_POST['grade'];
+		}
+		if($_POST['degree']!=''){
+			$condition['degree']=$_POST['degree'];
+		}
+		if($_POST['tutor']!=''){
+			$condition['tutor']=$_POST['tutor'];
+		}
+		$condition['tel']=$_POST['tel'];
+		//判断身份是否修改， 修改为教师后，取消部分
+		if($_POST['job']!=''){
+			$condition['job']=$_POST['job'];
+		}else if($_POST['job']==1){
+			$condition['job']='';
+			$condition['degree']='';
+			$condition['tutor']='';
 		}else{
-			$sys['role']=$role;
-			$m->save($sys);
-			$this->redirect('userview');
-		};
+			$condition['job']=$_POST['job'];
+		}
+	
+		$lastId=$m->where($condition_user)->save($condition);
+		if($lastId){
+			$this->success('修改成功');
+		}else{
+			$this->error('修改失败');
+		}
 	}
 }
